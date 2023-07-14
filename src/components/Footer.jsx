c141dfcd392d8c6e6275b558810d0743d9d0e814
sanglap kundu <kundusanglap02@gmail.com>
2023-07-15 01:50:31 +0530

    temp

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
             textAlign={["center", "left"]}
           >
             We are the best crypto trading app in India, we provide our guidance
-            at a very cheap price.
+            at a very cheap prices.
           </Text>
         </VStack>
 
